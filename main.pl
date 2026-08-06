bc6e9d89e473cd1af71e1f90d3e06a7bfc66977c
Connor White <YuMingC233@gmail.com>
2026-08-06 11:03:04 +0800

    pass: test 05

--- main.pl
+++ main.pl
@@ -18,9 +18,19 @@ use warnings;
 # }
 # print($sum)
 
-my $line = <STDIN>; chomp $line;
-my %counts;
-$counts{$_}++ for split /\s+/, $line;
-for my $val (sort keys %counts) {
-    print "$val: $counts{$val}\n";
+# my $line = <STDIN>; chomp $line;
+# my %counts;
+# $counts{$_}++ for split /\s+/, $line;
+# for my $val (sort keys %counts) {
+#     print "$val: $counts{$val}\n";
+# }
+
+my $line =  <STDIN>; chomp $line;
+my @numbers = ($line =~ /\d+/g);
+for (my $i = 0; $i < @numbers; $i++) {
+    if ($i == @numbers - 1) {
+        print "$numbers[$i]";
+        return;
+    }
+    print "$numbers[$i] ";
 }
