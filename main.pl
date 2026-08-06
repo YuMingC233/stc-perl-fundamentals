c5f7ce31065d20de12c30c82fa7a258ef81c336c
Connor White <YuMingC233@gmail.com>
2026-08-06 10:36:54 +0800

    pass: test 03

--- main.pl
+++ main.pl
@@ -1,8 +1,19 @@
 use strict;
 use warnings;
 
-my $a = <STDIN>; chomp $a;
-my $b = <STDIN>; chomp $b;
+# my $a = <STDIN>; chomp $a;
+# my $b = <STDIN>; chomp $b;
 
-print $a + $b;
+# print $a + $b;
 
+my $n = <STDIN>; chomp $n;
+my @nums;
+for (1..$n) {
+    my $x = <STDIN>; chomp $n;
+    push @nums, $x;
+}
+my $sum;
+for (my $i = 0; $i < scalar(@nums); $i++) {
+    $sum += $nums[$i];
+}
+print($sum)
